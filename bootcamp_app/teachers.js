8690e9e348872c8da01c708c486e73d00255a174
Kai Wingfield <kaiwingfield17@gmail.com>
2024-02-17 13:32:31 -0800

    Add parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teachers.name;
-`)
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name;
+  `;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
-  })
+  });
+  pool.end();
 }).catch(err => console.error('query error', err.stack));
